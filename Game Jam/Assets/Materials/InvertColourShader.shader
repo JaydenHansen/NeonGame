7fb8520edf0e96d4a70e0c757229ef98c81ddc50
T-Scod <36467730+T-Scod@users.noreply.github.com>
2019-05-23 11:40:25 +1000

    Invert colour shader stuff

---
+++ Game Jam/Assets/Materials/InvertColourShader.shader
@@ -0,0 +1,41 @@
+﻿Shader "Custom/InvertColourShader"
+{
+	Properties
+	{
+		TintColor("Tint Color", Color) = (1,1,1,1)
+	}
+		SubShader
+	{
+		Pass
+		{
+			Tags
+			{
+				"Queue" = "Overlay+100" "IgnoreProjector" = "True"
+			}
+
+			Blend OneMinusDstColor OneMinusSrcColor
+			ColorMask RGB
+
+			ZWrite Off
+			ZTest Always
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			float4 vert(float4 vertex : POSITION) : SV_Position
+			{
+				return UnityObjectToClipPos(vertex);
+			}
+
+			fixed4 TintColor;
+
+			fixed4 frag() : SV_Target
+			{
+				return TintColor;
+			}
+
+			ENDCG
+		}
+	}
+}
---
+++ Game Jam/Assets/Scripts/InvertColours.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvertColours : MonoBehaviour
+{
+    public Material material;
+    private bool m_isInverted = false;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (!m_isInverted)
+            {
+                material.SetColor("TintColor", Color.black);
+                m_isInverted = true;
+            }
+            else
+            {
+                material.SetColor("TintColor", Color.white);
+                m_isInverted = false;
+            }
+        }
+    }
+}
