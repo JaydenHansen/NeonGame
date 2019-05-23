d4c7773073a3646c8b48bfdec7e477644b2a5c42
T-Scod <36467730+T-Scod@users.noreply.github.com>
2019-05-23 12:31:18 +1000

    Implemented colour inversion in Tyson Scene.unity

--- Game Jam/Assets/Scripts/InvertColours.cs
+++ Game Jam/Assets/Scripts/InvertColours.cs
@@ -5,22 +5,38 @@ using UnityEngine;
 public class InvertColours : MonoBehaviour
 {
     public Material material;
+    public float invertDuration = 10.0f;
+
     private bool m_isInverted = false;
+    private float m_timer = 0;
 
     private void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (m_isInverted)
         {
-            if (!m_isInverted)
+            m_timer += Time.deltaTime;
+            if (m_timer >= invertDuration)
             {
                 material.SetColor("TintColor", Color.black);
-                m_isInverted = true;
-            }
-            else
-            {
-                material.SetColor("TintColor", Color.white);
                 m_isInverted = false;
+                m_timer = 0;
             }
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            return;
+        }
+        material.SetColor("TintColor", Color.white);
+        m_isInverted = true;
+    }
+
+    private void OnApplicationQuit()
+    {
+        material.SetColor("TintColor", Color.black);
+        m_isInverted = false;
     }
 }
