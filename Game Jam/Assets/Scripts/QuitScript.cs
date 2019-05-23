566180a29f11a75ce3e53d90ddbe94f69c432127
JaydenHansen <39174579+JaydenHansen@users.noreply.github.com>
2019-05-24 09:03:05 +1000

    NewAnimation and menu scene works

---
+++ Game Jam/Assets/Scripts/QuitScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class QuitScript : MonoBehaviour {
+
+    // Use this for initialization
+   public  void ButtonClicked(int buttonNo)
+    { 
+        
+        Application.Quit();
+    }
+}
---
+++ Game Jam/Assets/Scripts/Restart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+    }
+
+    public void changemenuscene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+   
+}
