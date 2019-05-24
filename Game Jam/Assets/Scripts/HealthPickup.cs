7299c445f1f25580c80041774853777108c19bda
T-Scod <36467730+T-Scod@users.noreply.github.com>
2019-05-24 11:00:38 +1000

    Added health pickups

---
+++ Game Jam/Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Restores player health on contact
+/// </summary>
+public class HealthPickup : MonoBehaviour
+{
+    /// <summary>
+    /// The amount of health restored on contact.
+    /// </summary>
+    public int healthRestoreAmount = 10;
+
+    /// <summary>
+    /// Passes the restore amount into the player restore health function on collision with a player.
+    /// </summary>
+    /// <param name="other">The object being collided with.</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        // checks if the object is a player
+        if (other.tag == "Player")
+        {
+            other.GetComponent<PlayerController>().RestoreHealth(healthRestoreAmount);
+            Destroy(gameObject);
+        }
+    }
+}
